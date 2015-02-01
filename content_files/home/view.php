1aa62bcd920b5e5900cabe509a00bb6de6367370
Javad <J.Evazzadeh@live.com>
2015-02-01 18:24:30 +0330

    add font-awesome to files and some other change

--- content_files/home/view.php
+++ content_files/home/view.php
@@ -5,13 +5,14 @@ class view extends \mvc\view
 {
 	public function config()
 	{
-		$this->data->bodyclass = 'fixed';
-		$this->include->js     = false;
-		$this->data->location  = $this->url('path', -1);
-
+		$this->data->bodyclass      = 'fixed';
+		$this->include->js          = false;
+		$this->include->fontawesome = true;
+		$this->data->location       = $this->url('path', -1);
+		
 		// var_dump($this->data->location);
-
-		$this->data->list      = $this->model()->directories($this->data->location);
+		
+		$this->data->list           = $this->model()->directories($this->data->location);
 
 		// var_dump($this->data->list);
 	}
